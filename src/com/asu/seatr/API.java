c005cb34511894bda138d2a8f0e5304fe2b461e5
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2016-01-21 16:08:11 -0700

    working rest api on java - jersey

---
+++ src/com/asu/seatr/API.java
@@ -0,0 +1,34 @@
+package com.asu.seatr;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Sets the path to base URL + /hello
+@Path("/hello")
+public class API {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Sample> sayJSONHello() {
+		
+		Sample ts = new Sample();
+        ts.setId("1");
+        ts.setVal("HelloWorld");
+        
+        Sample ts2 = new Sample();
+        ts2.setId("2");
+        ts2.setVal("HelloASU");
+
+        ArrayList<Sample> availSamples = new ArrayList<Sample>();
+        availSamples.add(ts);
+        availSamples.add(ts2);
+
+        return availSamples;
+	}
+ 
+
+} 
