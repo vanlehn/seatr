b7ff01bb22e398b6c7203c695d3c6ba93741d02c
Lishan Zhang <lishan.zhang@asu.edu>
2016-02-26 16:30:28 -0700

    Add course analyzer mapping if there is no mapping before

--- src/main/java/com/asu/seatr/handlers/AnalyzerHandler.java
+++ src/main/java/com/asu/seatr/handlers/AnalyzerHandler.java
@@ -1,94 +1,43 @@
 package com.asu.seatr.handlers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.metadata.ClassMetadata;
 
-import com.asu.seatr.models.interfaces.CourseAnalyzerI;
+import com.asu.seatr.exceptions.AnalyzerException;
+import com.asu.seatr.exceptions.CourseException;
+import com.asu.seatr.models.Analyzer;
 import com.asu.seatr.persistence.HibernateUtil;
+import com.asu.seatr.utils.MyMessage;
+import com.asu.seatr.utils.MyStatus;
 
-public class AnalyzerHandler<T> {
-
-	private static AnalyzerHandler analyzerHandler;
-	
-	public static AnalyzerHandler getInstance(){
-		if(analyzerHandler == null){
-			analyzerHandler = new AnalyzerHandler();
+public class AnalyzerHandler {
+	public static Analyzer getByName(String name) throws AnalyzerException{
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		Criteria cr = session.createCriteria(Analyzer.class);
+		cr.add(Restrictions.eq("name", name));
+		List<Analyzer> result = cr.list();
+		session.close();
+		if (result.size() == 0) {
+			throw new AnalyzerException(MyStatus.ERROR, MyMessage.ANALYZER_NOT_FOUND);
 		}
-		return analyzerHandler;
-	}
-	public int save(T analyzer) {
-	    SessionFactory sf = HibernateUtil.getSessionFactory();
-	    Session session = sf.openSession();
-	    session.beginTransaction();
-	    
-	    int id = (int)session.save(analyzer);
-	    //courseAnalyzer.setId(id);
-	    session.getTransaction().commit();
-	    session.close();
-	    return id;
+		else 
+			return result.get(0);
 	}
 	
-	public T read(int id, Class <T> typeParameterClass)
-	{
+	public static Analyzer getById(int id) throws AnalyzerException{
 		SessionFactory sf = HibernateUtil.getSessionFactory();
 		Session session = sf.openSession();
-		@SuppressWarnings("unchecked")
-		T analyzer = (T)session.get(typeParameterClass, id);
+		Analyzer analyzer=session.get(Analyzer.class, id);
 		session.close();
-		return analyzer;
-	}
-
-	public List<T> readByCriteria(Class <T> typeParameterClass, HashMap<String, Object> eqRestrictions) {
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-		Criteria cr = session.createCriteria(typeParameterClass);
-		cr.add(Restrictions.allEq(eqRestrictions));
-		List<T> result = cr.list();
-		session.close();
-		return result;		
+		if(analyzer==null)
+			throw new AnalyzerException(MyStatus.ERROR, MyMessage.ANALYZER_NOT_FOUND);
+		else
+			return analyzer;
 	}
 	
-	public List<T> readAll(String tableName)
-	{
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-		@SuppressWarnings("unchecked")
-		List<T> records = session.createQuery("from " + tableName).list();
-		session.close();
-		return records;
-	}
-	
-	public T update(T analyzer)
-	{
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();
-		session.merge(analyzer);
-		session.getTransaction().commit();
-		session.close();
-		return analyzer;
-	}
-	public void delete(T analyzer)
-	{
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();
-		session.delete(analyzer);
-		session.getTransaction().commit();
-		session.close();
-	}
-	public String[] getColumnNames(Class<T> typeParameterClass)
-	{
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		ClassMetadata classMetadata = sf.getClassMetadata(typeParameterClass);
-		String propertyNames[] = classMetadata.getPropertyNames();
-		return propertyNames;
-	}
 }
