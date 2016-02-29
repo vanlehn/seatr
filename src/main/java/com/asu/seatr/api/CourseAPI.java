2c5858b25a658d806258a2c89d44edf7f4b72b8c
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2016-02-28 20:04:00 -0700

    removed constraint violation exception handling at api level since its already handled

--- src/main/java/com/asu/seatr/api/CourseAPI.java
+++ src/main/java/com/asu/seatr/api/CourseAPI.java
@@ -127,12 +127,7 @@ public class CourseAPI {
 			Response rb = Response.status(Status.OK)
 					.entity(MyResponse.build(e.getMyStatus(), e.getMyMessage())).build();			
 			throw new WebApplicationException(rb);
-		} 
-		catch(ConstraintViolationException cve) {
-			Response rb = Response.status(Status.OK)
-					.entity(MyResponse.build(MyStatus.ERROR, MyMessage.COURSE_ALREADY_PRESENT)).build();
-			throw new WebApplicationException(rb);
-		}
+		} 		
 		catch(Exception e){
 			System.out.println(e.getMessage());
 			Response rb = Response.status(Status.BAD_REQUEST)
