c530700ee54a0d9d1f43e3fba3c47a11d26f18cf
Lishan Zhang <lishan.zhang@asu.edu>
2016-03-16 17:22:17 -0700

    run jmeter from java code

--- src/test/java/com/asu/seatr/api/test/jmeter/LoadTest.java
+++ src/test/java/com/asu/seatr/api/test/jmeter/LoadTest.java
@@ -2,10 +2,13 @@ package com.asu.seatr.api.test.jmeter;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.commons.io.IOUtils;
+import org.apache.jmeter.config.Argument;
+import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.control.LoopController;
 import org.apache.jmeter.control.gui.LoopControlPanel;
 import org.apache.jmeter.control.gui.TestPlanGui;
@@ -25,64 +28,59 @@ import org.apache.jorphan.collections.HashTree;
 import org.json.*;
 
 public class LoadTest {
-	public void readRequestFrom(String path) throws IOException{
-		File f=new File(path);
-		InputStream is=new FileInputStream(f);
-		String jsonTxt=IOUtils.toString(is);
-		JSONArray jsonArray=new JSONArray(jsonTxt);
+	private String jmeterHome="C:\\apache-jmeter-2.11\\";
+	private String jmeterProperty="C:\\apache-jmeter-2.11\\bin\\jmeter.properties";
+	
+	// JMeter Engine
+	StandardJMeterEngine jmeter = new StandardJMeterEngine();
+	// Test Plan
+    TestPlan testPlan = new TestPlan("Create JMeter Script From Java Code");
+	// JMeter Test Plan, basic all u JOrphan HashTree
+    HashTree testPlanTree = new HashTree();
+    // Thread Group
+    ThreadGroup threadGroup = new ThreadGroup();
+    // Loop Controller
+    LoopController loopController = new LoopController();
+    
+    HashTree threadGroupHashTree;
+	
+	private void initJmeterPlan(){
 		
-		// JMeter Engine
-		StandardJMeterEngine jmeter = new StandardJMeterEngine();
 		
 		 // Initialize Properties, logging, locale, etc.
-		JMeterUtils.setJMeterHome("C:\\apache-jmeter-2.11\\");
-        JMeterUtils.loadJMeterProperties("C:\\apache-jmeter-2.11\\bin\\jmeter.properties");
-        //JMeterUtils.initLogging();// you can comment this line out to see extra log messages of i.e. DEBUG level
+		JMeterUtils.setJMeterHome(jmeterHome);
+        JMeterUtils.loadJMeterProperties(jmeterProperty);
+        JMeterUtils.initLogging();// you can comment this line out to see extra log messages of i.e. DEBUG level
         JMeterUtils.initLocale();
-        
-        // JMeter Test Plan, basic all u JOrphan HashTree
-        HashTree testPlanTree = new HashTree();
-        
-        // HTTP Sampler
-        HTTPSamplerProxy httpSampler = new HTTPSamplerProxy ();
-        httpSampler.setDomain("ec2-52-35-118-9.us-west-2.compute.amazonaws.com");
-        httpSampler.setPort(8080);
-        httpSampler.setPath("/seatr/rest/analyzer/1/students?external_student_id=22&external_course_id=37");
-        httpSampler.setMethod("GET");
-        httpSampler.setName("Test 1");
-        httpSampler.setProperty(TestElement.TEST_CLASS, HTTPSamplerProxy.class.getName());
-        httpSampler.setProperty(TestElement.GUI_CLASS, HttpTestSampleGui.class.getName());
-        
-        // Loop Controller
-        LoopController loopController = new LoopController();
+    
         loopController.setLoops(1);
         loopController.setFirst(true);
         loopController.setProperty(TestElement.TEST_CLASS, LoopController.class.getName());
         loopController.setProperty(TestElement.GUI_CLASS, LoopControlPanel.class.getName());
         loopController.initialize();
         
-        // Thread Group
-        ThreadGroup threadGroup = new ThreadGroup();
         threadGroup.setName("Sample Thread Group");
         threadGroup.setNumThreads(1);
         threadGroup.setRampUp(1);
         threadGroup.setSamplerController(loopController);
         threadGroup.setProperty(TestElement.TEST_CLASS, ThreadGroup.class.getName());
         threadGroup.setProperty(TestElement.GUI_CLASS, ThreadGroupGui.class.getName());
-        
-        // Test Plan
-        TestPlan testPlan = new TestPlan("Create JMeter Script From Java Code");
+            
         testPlan.setProperty(TestElement.TEST_CLASS, TestPlan.class.getName());
         testPlan.setProperty(TestElement.GUI_CLASS, TestPlanGui.class.getName());
         
         // Construct Test Plan from previously initialized elements
-        testPlanTree.add("testPlan", testPlan);
-        HashTree threadGroupHashTree = testPlanTree.add(testPlan, threadGroup);
-        threadGroupHashTree.add(httpSampler);
-        // save generated test plan to JMeter's .jmx file format
+        testPlanTree.add(testPlan);
+        threadGroupHashTree = testPlanTree.add(testPlan, threadGroup);
+	}
+	
+	private void savePlan() throws FileNotFoundException, IOException{
+		// save generated test plan to JMeter's .jmx file format
         SaveService.saveTree(testPlanTree, new FileOutputStream(".\\jmeter\\jmeter_api_sample.jmx"));
-        
-        //add Summarizer output to get test progress in stdout like:
+	}
+	
+	public void runTest(){
+		//add Summarizer output to get test progress in stdout like:
         // summary =      2 in   1.3s =    1.5/s Avg:   631 Min:   290 Max:   973 Err:     0 (0.00%)
         Summariser summer = null;
         String summariserName = JMeterUtils.getPropDefault("summariser.name", "summary");
@@ -104,11 +102,52 @@ public class LoadTest {
         // Run Test Plan
         jmeter.configure(testPlanTree);
         jmeter.run();
+	}
+
+	
+	public void readRequestFrom(String path) throws IOException{
+		File f=new File(path);
+		InputStream is=new FileInputStream(f);
+		String jsonTxt=IOUtils.toString(is);
+		JSONArray jsonArray=new JSONArray(jsonTxt);
 		
-		/*for(int i=0;i<jsonArray.length();i++){
+		this.initJmeterPlan();
+        
+        
+        
+        
+		int e_course_id=37;
+		System.out.println(jsonArray.length());
+		for(int i=0;i<jsonArray.length();i++){
 			JSONObject json=jsonArray.getJSONObject(i);
-			System.out.println(json);
-		}*/
+			
+			JSONObject body=new JSONObject();
+			body.put("external_student_id", String.valueOf(json.get("user_id")));
+			body.put("external_course_id", e_course_id);
+			body.put("external_task_id", String.valueOf(json.get("task_id")));
+			body.put("d_status","done");
+			body.put("d_time_lastattempt", -1);
+			
+			// HTTP Sampler
+	        HTTPSamplerProxy httpSampler = new HTTPSamplerProxy ();
+	        httpSampler.setDomain("ec2-52-35-118-9.us-west-2.compute.amazonaws.com");
+	        httpSampler.setPort(8080);
+	        httpSampler.setPath("/seatr/rest/analyzer/1/students?external_student_id=22&external_course_id=37");
+	        httpSampler.setMethod("POST");
+	        httpSampler.addArgument("", body.toString());
+	        httpSampler.setName("Test "+i);
+	        httpSampler.setProperty(TestElement.TEST_CLASS, HTTPSamplerProxy.class.getName());
+	        httpSampler.setProperty(TestElement.GUI_CLASS, HttpTestSampleGui.class.getName());
+	        threadGroupHashTree.add(httpSampler);
+	       
+	        if(i>1000)
+	        	break;
+		}
+		System.out.println("Saving test plan...");
+		this.savePlan();
+		System.out.println("Test plan is save.");
+		
+		//runTest();
 	}
 	
 	public static void main(String[] args) throws IOException{
