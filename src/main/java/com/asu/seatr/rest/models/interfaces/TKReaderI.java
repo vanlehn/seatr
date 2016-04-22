4c9c22f791462dee518fbd9f3ab19c7eab00fac3
dikshayp <dpoojary@asu.edu>
2016-04-22 12:14:54 -0700

    tkreaderi changed

--- src/main/java/com/asu/seatr/rest/models/interfaces/TKReaderI.java
+++ src/main/java/com/asu/seatr/rest/models/interfaces/TKReaderI.java
@@ -6,14 +6,8 @@ public interface TKReaderI {
 
 	public boolean getReplace();
 	public void setReplace(boolean replace);
-<<<<<<< HEAD
-	//public TKAReaderI[] getTkaReader();
-	//public void setTkaReader(TKAReaderI[] tkaReader);
 	public String getExternal_course_id();
 	public void setExternal_course_id(String external_course_id);
-=======
-	public TKAReaderI[] getTkaReader();
-	//public void setTkaReader(TKAReaderI[] tkaReader);
->>>>>>> 6d90b68ff7d83d005b0869286422949e06a7f8b5
+
 	
 }
