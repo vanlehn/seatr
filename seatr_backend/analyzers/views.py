2a354d129e2b1d14b5ea803e630b1b370fdf347c
Rishab Banerjee <crpbanerjee@gmail.com>
2019-02-08 15:53:00 -0700

    seatr_backend/questions/models.py: status field added whether the question is seen or solved; migration done sucessfully ie. tables in seatr-rds created and django migration files created

--- seatr_backend/analyzers/views.py
+++ seatr_backend/analyzers/views.py
@@ -3,10 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class Analyzer1(APIView):
     def post(self, request, format=None):
         questions = request.POST.get('questions', None)
-        questions = [x for x in range(10)]
         return Response({
             "questions": questions
         }, status=status.HTTP_200_OK)
--- seatr_backend/questions/models.py
+++ seatr_backend/questions/models.py
@@ -9,7 +9,14 @@ class Questions(models.Model):
     external_id = models.IntegerField()
 
 
+STATUS_CHOICES = (
+    (1, "solved"),
+    (2, "studied/seen"),
+)
+
+
 class QuestionsStudentMap(models.Model):
     students  = models.ForeignKey(studentsModel.Students, on_delete=models.PROTECT)
     questions = models.ForeignKey(Questions, on_delete=models.PROTECT)
+    status    = models.IntegerField(choices=STATUS_CHOICES, default=2)
 
